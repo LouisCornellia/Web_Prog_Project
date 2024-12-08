03f170a42a08853d2db0bb98f6e57858b2263a75
Louis Cornellia <lcornellia25@gmail.com>
2024-12-08 21:20:24 +0700

    second commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="{{ asset('bootstrap/bootstrap-5.3.3-dist/css/bootstrap.min.css') }}">
+    <style>
+        .banner-img {
+            width: 100%;
+            height: 500px;
+        }
+    </style>
+    <title>FinFo</title>
+</head>
+<body>
+
+
+
+
+    <script src="{{ asset('bootstrap/bootstrap-5.3.3-dist/js/bootstrap.bundle.min.js') }}" defer></script>
+</body>
+</html>
